c86dea7377906d8727d7276ef92a9948b5aefef3
RenanGAS <renansakaoki@gmail.com>
2023-04-30 22:51:06 -0300

    chore: add the attribute length to the Text_Field

--- entrada.rb
+++ entrada.rb
@@ -1,30 +1,19 @@
 require_relative 'model'
 require_relative 'generator'
 
-# class Learning(models.Model):
-#   id = models.BigAutoField(primary_key=True)
-#   name = models.CharField(max_length=128, blank=False)
-#   course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='learnings', null=False, blank=False)
-
 class Pessoa
-    attr_accessor :teste, :teste2
     def initialize
-      #Text_field tem parametro opcional, vazio ele usa o valro default, ou então faz passando como parametro nomeado
-      #@teste3 = Text_Field.new(:placeholder => 'red')
-      @Nome = Text_Field.new(:placeholder => "Nome", :verbose_name => "Seu nome")
-      @cpf = Text_Field.new(:placeholder => "CPF", :verbose_name => "cpf")
+      @nome = Text_Field.new(:placeholder => "Nome", :verbose_name => "Nome", :length => 50)
+      @raca = Text_Field.new(:placeholder => "CPF", :verbose_name => "cpf", :length => 50)
     end
 end
 
 class Carro
-  attr_accessor :teste, :teste2
   def initialize
-    #Text_field tem parametro opcional, vazio ele usa o valro default, ou então faz passando como parametro nomeado
-    #@teste3 = Text_Field.new(:placeholder => 'red')
-    @nomeDoCarro = Text_Field.new(:placeholder => "Nome do carro.")
-    @dono = ForeignKey.new(:with => Pessoa)
+    @marca = Text_Field.new(:placeholder => "Marca", :length => 50)
+    @cor = Text_Field.new(:placeholder => "Cor", :length => 50)
+    @pessoa = ForeignKey_Field.new(:with => Pessoa)
   end
 end
-  
   
 register(Pessoa.new, Carro.new)
