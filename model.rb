67a93034b3ff44779ad3cc096368cfc92e768143
RenanGAS <renansakaoki@gmail.com>
2023-05-02 02:15:15 -0300

    feat: add the type attribute

--- model.rb
+++ model.rb
@@ -1,22 +1,26 @@
 require 'sqlite3'
 
 class ForeignKey_Field
-  attr_accessor :with
-  def initialize(with: "None")
+  attr_accessor :with, :reference_field
+  def initialize(with: "None", referencing: 'id')
     if with.class.name == "None"
       raise ArgumentError, "with argument is required."
     end
-
     @with = with.to_s
+    @reference_field = referencing
   end
 end
 
-
 class Text_Field
-  attr_accessor :placeholder, :verbose, :length
-  def initialize(placeholder: '', verbose_name:'', length: 50)
+  attr_accessor :placeholder, :verbose, :length, :type
+  def initialize(placeholder: '', verbose_name:'', length: 50, type: 'string')
     @placeholder = placeholder
     @verbose = verbose_name
     @length = length
+    if(type.downcase == 'string')
+      @type = "text"
+    else(type.downcase == 'int')
+        @type = "number"
+    end
   end
 end
