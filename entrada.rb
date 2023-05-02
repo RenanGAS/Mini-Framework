330ad99732438ab5fd802a0f372918b9f26f83c1
RenanGAS <renansakaoki@gmail.com>
2023-05-02 02:13:22 -0300

    feat: add the type attribute

--- entrada.rb
+++ entrada.rb
@@ -4,16 +4,24 @@ require_relative 'generator'
 class Pessoa
     def initialize
       @nome = Text_Field.new(:placeholder => "Nome", :verbose_name => "Nome", :length => 50)
-      @raca = Text_Field.new(:placeholder => "CPF", :verbose_name => "cpf", :length => 50)
+      @cpf = Text_Field.new(:placeholder => "CPF", :verbose_name => "cpf", :length => 50)
     end
+end
+
+class Produto
+  def initialize
+    @nome = Text_Field.new(:placeholder => "Produto", :length => 50)
+    @quantidade = Text_Field.new(:placeholder => "Valor", :type => 'int')
+    @pessoa = ForeignKey_Field.new(:with => Pessoa, :referencing => 'nome')
+  end
 end
 
 class Carro
   def initialize
     @marca = Text_Field.new(:placeholder => "Marca", :length => 50)
     @cor = Text_Field.new(:placeholder => "Cor", :length => 50)
-    @pessoa = ForeignKey_Field.new(:with => Pessoa)
+    @pessoa = ForeignKey_Field.new(:with => Pessoa, :referencing => 'nome')
   end
 end
   
-register(Pessoa.new, Carro.new)
+register(Pessoa.new, Carro.new, Produto.new)
